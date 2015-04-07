56250cd2e378fabf70f57b4ac13ed995ad539cfe
dtaniwaki <daisuketaniwaki@gmail.com>
2015-04-07 09:09:12 +0900

    Use nan to abstract node versions

--- srand.cc
+++ srand.cc
@@ -1,34 +1,34 @@
 
 #include <stdlib.h>
 #include <v8.h>
+#include <nan.h>
 #include <node.h>
 
 using namespace node;
 using namespace v8;
 
-static Handle<Value> Random (const Arguments&);
-static Handle<Value> Rand (const Arguments&);
-static Handle<Value> SRand (const Arguments&);
 extern "C" void init (Handle<Object>);
 
-static Handle<Value> Random (const Arguments& args) {
-  return Number::New(rand()/((double)RAND_MAX + 1));
+NAN_METHOD(Random) {
+  NanScope();
+  NanReturnValue(NanNew<Number>(rand()/((double)RAND_MAX + 1)));
 }
 
-static Handle<Value> Rand (const Arguments& args) {
-  return Integer::New(rand());
+NAN_METHOD(Rand) {
+  NanScope();
+  NanReturnValue(NanNew<Integer>(rand()));
 }
 
-static Handle<Value> SRand (const Arguments& args) {
+NAN_METHOD(SRand) {
+  NanScope();
   if (args.Length() == 0) {
-    return ThrowException(Exception::Error(String::New("Usage: srand(n)")));
+    NanThrowError(Exception::Error(NanNew<String>("Usage: srand(n)")));
   }
   srand(args[0]->Uint32Value());
-  return Undefined();
+  NanReturnValue(NanUndefined());
 }
 
 extern "C" void init (Handle<Object> target) {
-  HandleScope scope;
   NODE_SET_METHOD(target, "random", Random);
   NODE_SET_METHOD(target, "rand", Rand);
   NODE_SET_METHOD(target, "srand", SRand);
